33261a32c2f9affd901b5b3a07fb4b9d2c65a3af
rykhlo <iurii.rykhlo@utexas.edu>
2021-05-26 01:41:22 -0500

    Removed unused import

--- auctions/views.py
+++ auctions/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.db.models import Max
 
 
 from .models import *
